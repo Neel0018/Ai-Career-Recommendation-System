23daf6145efad3f544e6695b9015eaf6c9fdf898
Akill257 <kapilsouravrout04@gmail.com>
2026-03-07 21:58:55 +0530

    Update app.py
    More secure + fix missing career

--- backend/app.py
+++ backend/app.py
@@ -3,15 +3,21 @@ from flask_cors import CORS
 from model import recommend_career
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["http://localhost:5173"])
 
 @app.route("/recommend", methods=["POST"])
 def recommend():
 
     data = request.json
 
+    if not data:
+        return jsonify([])
+
     interest = data.get("interest")
     performance = data.get("performance")
+
+    if not interest or not performance:
+        return jsonify([])
 
     result = recommend_career(interest, performance)
 
